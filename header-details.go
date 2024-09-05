d984d99b7eca9fe1e0756ffb8c36cc3d63b2ac08
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2024-09-05 18:39:03 -0400

    header detail refactor

--- header-details.go
+++ header-details.go
@@ -2,8 +2,6 @@ package pdf_invoice
 
 func (h *handler) generateHeaderDetails() error {
 
-	// h.pdf.SetFont(h.font_normal, "", 9)
-
 	const box_start = 135
 	// INICIO RECTÁNGULO ROJO DESCRIPCIÓN
 	err := h.pdf.Rectangle(h.margin_left, box_start, 581.4, 228, "D", 0, 0)
@@ -14,36 +12,30 @@ func (h *handler) generateHeaderDetails() error {
 	const margin = 5
 	var margin_top float64 = box_start + margin
 	var margin_left = h.margin_left + margin*2
-	h.pdf.SetXY(margin_left, margin_top)
 
+	// CLIENT CONTENT
 	labels := []string{"Señor(es)", "Giro", "Rut", "Dirección", "Comuna", "Teléfono", "Email"}
 	values := []string{h.invoice.ClientName, h.invoice.ClientActivity, h.invoice.ClientRUT, h.invoice.ClientAddress, h.invoice.ClientCity, h.invoice.ClientPhone, h.invoice.ClientEmail}
 
-	y := margin_top
-	label_width := 75.0
-	line_break := 12.0
+	h.makeHeaderDetailsContent(margin_top, margin_left, labels, values)
 
-	for i, label := range labels {
-		/// establecer la etiqueta
-		h.pdf.SetXY(margin_left, y)
-		h.pdf.Cell(nil, label)
-
-		// establecer  dos puntos :
-		h.pdf.SetXY(margin_left+label_width, y)
-		h.pdf.Cell(nil, ":")
-
-		// establecer el valor
-		h.pdf.SetXY(margin_left+label_width+10, y)
-		h.pdf.Cell(nil, values[i])
-
-		y += line_break
-	}
-
+	// INVOICE CONTENT
 	labels = []string{"Fecha Emisión", "Medio de Pago", "Vendedor"}
 	values = []string{h.invoice.Date, h.invoice.PaymentMethod, h.invoice.Seller}
 
-	y = margin_top
 	margin_left = 397.8
+
+	h.makeHeaderDetailsContent(margin_top, margin_left, labels, values)
+
+	h.setDefaultFont()
+	return nil
+}
+
+func (h *handler) makeHeaderDetailsContent(y, margin_left float64, labels, values []string) {
+	const (
+		label_width = 75.0
+		line_break  = 12.0
+	)
 
 	for i, label := range labels {
 		/// establecer la etiqueta
@@ -60,7 +52,4 @@ func (h *handler) generateHeaderDetails() error {
 
 		y += line_break
 	}
-
-	h.setDefaultFont()
-	return nil
 }
